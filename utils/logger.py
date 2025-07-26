df78455ed528fd9e5945141914f78d7034362b09
kamaleshpati <k97pati@gmail.com>
2025-07-26 20:28:33 +0530

    start again

--- utils/logger.py
+++
@@ -1,7 +0,0 @@
-# utils/logger.py
-
-from datetime import datetime
-
-def log(message):
-    """Logs a message with timestamp."""
-    print(f"[{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}] {message}")
